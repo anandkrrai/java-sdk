a6e711f27c38fff1a66e426ff5d908469627f6c0
Rai <nndri@amazon.com>
2025-07-17 22:42:04 +0530

    formatting fix

--- mcp/src/test/java/io/modelcontextprotocol/testtss.java
+++ mcp/src/test/java/io/modelcontextprotocol/testtss.java
@@ -6,28 +6,34 @@ import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport;
 import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport;
 
 public class testtss {
-    @org.junit.jupiter.api.Test
-    public void test() {
 
-//        HttpClientSseClientTransport httpClientSseClientTransport = HttpClientSseClientTransport.builder("https://docs.mcp.cloudflare.com/sse").build();
-//        McpSyncClient mcpSyncClient = McpClient.sync(httpClientSseClientTransport).build();
-//        mcpSyncClient.initialize();
-//        mcpSyncClient.listTools();
+	@org.junit.jupiter.api.Test
+	public void test() {
 
+		// HttpClientSseClientTransport httpClientSseClientTransport =
+		// HttpClientSseClientTransport.builder("https://docs.mcp.cloudflare.com/sse").build();
+		// McpSyncClient mcpSyncClient =
+		// McpClient.sync(httpClientSseClientTransport).build();
+		// mcpSyncClient.initialize();
+		// mcpSyncClient.listTools();
 
-        System.out.println("********************");
+		System.out.println("********************");
 
-//        HttpClientSseClientTransport httpClientSseClientTransport = HttpClientSseClientTransport.builder("https://mcp.atlassian.com/v1/sse").build();
-        HttpClientStreamableHttpTransport streamableHttpTransport = HttpClientStreamableHttpTransport.builder("https://qbiz-mcp.abhjaw.people.aws.dev").build();
-        HttpClientSseClientTransport sseHttpTransport = HttpClientSseClientTransport.builder("https://qbiz-mcp.abhjaw.people.aws.dev").build();
-//        HttpClientStreamableHttpTransport streamableHttpTransport = HttpClientStreamableHttpTransport.builder("https://mcp.atlassian.com/v1/sse").build();
-//
-        McpSyncClient client = McpClient.sync(streamableHttpTransport)
-            .build();
-        client.initialize();
+		// HttpClientSseClientTransport httpClientSseClientTransport =
+		// HttpClientSseClientTransport.builder("https://mcp.atlassian.com/v1/sse").build();
+		HttpClientStreamableHttpTransport streamableHttpTransport = HttpClientStreamableHttpTransport
+			.builder("https://qbiz-mcp.abhjaw.people.aws.dev")
+			.build();
+		HttpClientSseClientTransport sseHttpTransport = HttpClientSseClientTransport
+			.builder("https://qbiz-mcp.abhjaw.people.aws.dev")
+			.build();
+		// HttpClientStreamableHttpTransport streamableHttpTransport =
+		// HttpClientStreamableHttpTransport.builder("https://mcp.atlassian.com/v1/sse").build();
+		//
+		McpSyncClient client = McpClient.sync(streamableHttpTransport).build();
+		client.initialize();
 
-
-        client.listTools();
-    }
+		client.listTools();
+	}
 
 }
